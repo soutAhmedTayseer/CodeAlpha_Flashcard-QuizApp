e835aea910db153b025ccc01d801d723fdddb1f4
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2024-09-10 15:01:24 +0300

    states initialization

---
+++ lib/states.dart
@@ -0,0 +1,21 @@
+abstract class AppStates {}
+
+class AppInitialState extends AppStates {}
+
+class AppLoginLoadingState extends AppStates {}
+
+class AppLoginSuccessState extends AppStates {}
+
+class AppLoginErrorState extends AppStates {}
+
+class AppSignupLoadingState extends AppStates {}
+
+class AppSignupSuccessState extends AppStates {}
+
+class AppSignupErrorState extends AppStates {}
+
+class AppAddFlashcardState extends AppStates {}
+
+class AppGetFlashcardsState extends AppStates {}
+
+class AppQuizCompleteState extends AppStates {}
